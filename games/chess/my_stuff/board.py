cef7785a8b772550ad6c70c87a45774de286a06c
johnmaruska <jwmbq6@mst.edu>
2017-02-28 13:01:37 -0600

    Fixed issue with board generation. Implemented Board.copy()

--- games/chess/my_stuff/board.py
+++ games/chess/my_stuff/board.py
@@ -7,40 +7,46 @@ class Board(list):
         super().__init__()
         self.width = 8
         self.height = 8
-        self.generate_by(fen)
+        if fen is not None:
+            self.generate_by(fen)
+            self.fen = fen
+        else:
+            # Initial state
+            self.fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
     def copy(self):
-        # TODO: Implement
-        pass
+        return Board(self.fen)
 
-    def generate_by(self, fen):
+    def generate_by(self, fen=None):
         del self[:]
-        if fen is None:
-            self.append(["r", "n", "b", "q", "k", "b", "n", "r"])
-            self.append(["p", "p", "p", "p", "p", "p", "p", "p"])
-            self.append(["",  "",  "",  "",  "",  "",  "",  ""])
-            self.append(["",  "",  "",  "",  "",  "",  "",  ""])
-            self.append(["",  "",  "",  "",  "",  "",  "",  ""])
-            self.append(["",  "",  "",  "",  "",  "",  "",  ""])
-            self.append(["P", "P", "P", "P", "P", "P", "P", "P"])
-            self.append(["R", "N", "B", "Q", "K", "B", "N", "R"])
-        else:
+        if fen is not None:
             fen_pieces = fen.split(" ")[0]  # Strips FEN of non-location info
             row_lists = fen_pieces.split("/")
-            for r in range(row_lists):
-                rank = 8-r
-                file = 0  # file should go from 0-7 representing a-h
+            placeholder = []
+            for r in row_lists:
                 row = []
-                for c in r.split(""):
+                for c in r:
                     if c.isdigit():
-                        for _ in range(c):
+                        for _ in range(int(c)):
                             row.append("")
                     else:
                         row.append(c)
-                self.append(row)
+                placeholder.append(row)
             # This implementation actually puts ranks 8 descending in list first
             # [r8, r7, r6, r5, r4, r3, r2, r1]
             # Must be reversed to be able to address easily by rank.
             # [r1, r2, r3, r4, r5, r6, r7, r8]
-            self.reverse()
+            placeholder.reverse()
+            # Transpose to allow board[x][y] indexing instead of board[y][x]
+            placeholder = list(map(list, zip(*placeholder)))
+            [self.append(x) for x in placeholder]
+        # else:  # Initial state
+        #     self.append(["r", "n", "b", "q", "k", "b", "n", "r"])
+        #     self.append(["p", "p", "p", "p", "p", "p", "p", "p"])
+        #     self.append(["",  "",  "",  "",  "",  "",  "",  ""])
+        #     self.append(["",  "",  "",  "",  "",  "",  "",  ""])
+        #     self.append(["",  "",  "",  "",  "",  "",  "",  ""])
+        #     self.append(["",  "",  "",  "",  "",  "",  "",  ""])
+        #     self.append(["P", "P", "P", "P", "P", "P", "P", "P"])
+        #     self.append(["R", "N", "B", "Q", "K", "B", "N", "R"])
         pass
